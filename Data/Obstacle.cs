53451c984c80fdc18d5232020cb699fd0da8a373
Mathias Holmgaard <mathias.holmgaard@hotmail.com>
2024-04-24 15:32:30 +0200

    Add project files.

---
+++ Data/Obstacle.cs
@@ -0,0 +1,12 @@
+﻿namespace Data
+{
+    public class Obstacle
+    {
+        int ID {get; set;}  
+        string Name { get; set;}
+        bool IsSign { get; set;}
+        string Image { get; set;}
+
+
+    }
+}
